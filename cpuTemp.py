89ea8f8fb7817dd606d07290f7d768091b13cac5
Sam Deo <deolivs@gmail.com>
2024-10-22 04:34:56 +0100

    Nothing really

--- cpuTemp.py
+++ cpuTemp.py
@@ -2,7 +2,7 @@
 
 import subprocess, sys
 
-#Use this if you want to run it with a command line argument.
+#Use this if you want to run it with a command line argument. Add
 #command = sys.argv[1:]
 command = "vcgencmd measure_temp"
 
