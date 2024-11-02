1114ce26c81496f2b68f8db162fe6228ba717ff6
Sam Deo <deolivs@gmail.com>
2024-11-02 17:14:11 -0400

    Cut for just numbers

--- cpuTemp.py
+++ cpuTemp.py
@@ -4,6 +4,8 @@ import subprocess, sys
 
 #Use this if you want to run it with a command line argument. Add
 #command = sys.argv[1:]
-command = "vcgencmd measure_temp"
+
+#1st cut extracts tempt number with unit, second cut gets only first 4 bytes.
+command = r'vcgencmd measure_temp | cut -d= -f2 | cut -b 1-4'
 
 subprocess.run(command, shell = True, executable="/bin/bash")
